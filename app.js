82e170a19cedfdfd286d1127aa3fbc6c04369b12
Maddy246 <29030663+Maddy246@users.noreply.github.com>
2021-08-30 01:22:38 +0530

    css updated

--- app.js
+++ app.js
@@ -2,28 +2,30 @@ var checkbtn = document.querySelector("#btn-check");
 var input = document.querySelectorAll(".stock-inputs");
 var output = document.querySelector("#output");
 
-function checkProfitOrLoss(buyPrice, quantity, currentPrice) {
-
+function checkProfitOrLoss(buyPrice, quantity, currentPrice) {   
     if (buyPrice > 0 && quantity > 0 && currentPrice > 0) {
 
         if (currentPrice > buyPrice) {
             var profit = quantity * (currentPrice - buyPrice);
             var profitPercentage = (profit / buyPrice) * 100;
 
+            output.style.color = "green";
             output.innerText = "Your profit is " + profit +
-                " and it is " + profitPercentage + "%";
+                " and it is " + profitPercentage.toFixed(2) + "%";
 
         } else if (buyPrice > currentPrice) {
             var loss = quantity * (buyPrice - currentPrice);
             var lossPercentage = (loss / buyPrice) * 100;
 
+            output.style.color = "red";
             output.innerText = "Your loss is " + loss +
-                " and it is " + lossPercentage + "%";
+                " and it is " + lossPercentage.toFixed(2) + "%";
 
         } else {
             output.innerText = "You don't have any profit or loss...";
         }
     } else {
+        output.style.color = "red";
         output.innerText = "Please enter correct values in the above fields..";
     }
 
